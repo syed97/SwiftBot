21ccf5cf9f9ceaa2aa8242598a3b60a409f1a811
Syed Ahsan Ahmed <snahmed1998@gmail.com>
2019-09-17 18:28:29 +0500

    login and signup

--- Swift/src/app/app.module.ts
+++ Swift/src/app/app.module.ts
@@ -10,6 +10,9 @@ import { TabsPage } from '../pages/tabs/tabs';
 
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
+import { PservicesProvider } from '../providers/pservices/pservices';
+
+import {IonicStorageModule} from '@ionic/storage';
 
 @NgModule({
   declarations: [
@@ -21,6 +24,7 @@ import { SplashScreen } from '@ionic-native/splash-screen';
   ],
   imports: [
     BrowserModule,
+    IonicStorageModule.forRoot(),
     IonicModule.forRoot(MyApp)
   ],
   bootstrap: [IonicApp],
@@ -34,7 +38,8 @@ import { SplashScreen } from '@ionic-native/splash-screen';
   providers: [
     StatusBar,
     SplashScreen,
-    {provide: ErrorHandler, useClass: IonicErrorHandler}
+    {provide: ErrorHandler, useClass: IonicErrorHandler},
+    PservicesProvider
   ]
 })
 export class AppModule {}
